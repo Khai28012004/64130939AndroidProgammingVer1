be6d6077704f1b051664460824f2dfb72ca4fd4c
Khai28012004 <khai.ln.64cntt@ntu.edu.vn>
2025-03-05 16:05:30 +0700

    .

---
+++ Ex5_AddSubMulDiv_Anynomous/app/src/main/java/khai/edu/ex5_addsubmuldiv_anynomous/MainActivity.java
@@ -0,0 +1,147 @@
+package khai.edu.ex5_addsubmuldiv_anynomous;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //Khai báo các đối tượng gắn vơ đkhiển tương ứng
+    EditText edtS1;
+    EditText edtS2;
+    EditText edtKQ;
+
+    Button nutCong,nutTru,nutNhan,nutChia;
+    void TimDieuKhien(){
+        edtS1 = findViewById(R.id.edtSo1);
+        edtS2 = findViewById(R.id.edtSo2);
+        edtKQ = findViewById(R.id.edtKetQua);
+        nutCong = findViewById(R.id.btnCong);
+        nutTru = findViewById(R.id.btnTru);
+        nutNhan = findViewById(R.id.btnNhan);
+        nutChia = findViewById(R.id.btnChia);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TimDieuKhien();
+        //Gan bo lang nghe su kien va code xu ly cho tung nut
+        View.OnClickListener boLAngNgheCong = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyCong();
+            }
+        };
+        nutCong.setOnClickListener(boLAngNgheCong);
+        nutTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyTru();
+            }
+        });
+        nutNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyNhan();
+            }
+        });
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XuLyChia();
+            }
+        });
+    }
+
+    //Xử lý cộng
+    void XuLyCong() {
+        //Bước 1: Lấy dữ liệu
+        //Bước 1.1: Tìm và Tham chiếu dữ liệu
+
+        //Bước 1.2: Lấy dữ liệu từ 2 điều khiển
+        String soThuNhat = edtS1.getText().toString();
+        String soThuHai = edtS2.getText().toString();
+        //Bước 1.3: Chuyển dữ liệu từ chuỗi sang số
+        float s1 = Float.parseFloat(soThuNhat);
+        float s2 = Float.parseFloat(soThuHai);
+        //Bước 2: Tính toán
+        float Tong = s1 + s2;
+        //Bước 3: Hiển thị kết quả
+        //B3.1: Tìm và tham chiếu
+
+        //B3.2: Chuẩn bị dữ liệu xuất => chuyển KQ thành dạng chuỗi
+        String KQ = String.valueOf(Tong);
+        //B3.3: Gán kết quả lên điều khiển
+        edtKQ.setText(KQ);
+    }
+
+    //Xử lý trừ
+    void XuLyTru() {
+        //Bước 1: Lấy dữ liệu
+        //Bước 1.1: Tìm và Tham chiếu dữ liệu
+
+        //Bước 1.2: Lấy dữ liệu từ 2 điều khiển
+        String soThuNhat = edtS1.getText().toString();
+        String soThuHai = edtS2.getText().toString();
+        //Bước 1.3: Chuyển dữ liệu từ chuỗi sang số
+        float s1 = Float.parseFloat(soThuNhat);
+        float s2 = Float.parseFloat(soThuHai);
+        //Bước 2: Tính toán
+        float Hieu = s1 - s2;
+        //Bước 3: Hiển thị kết quả
+        //B3.1: Tìm và tham chiếu
+
+        //B3.2: Chuẩn bị dữ liệu xuất => chuyển KQ thành dạng chuỗi
+        String KQ = String.valueOf(Hieu);
+        //B3.3: Gán kết quả lên điều khiển
+        edtKQ.setText(KQ);
+    }
+
+    //Xử lý nhân
+    void XuLyNhan() {
+        //Bước 1: Lấy dữ liệu
+        //Bước 1.1: Tìm và Tham chiếu dữ liệu
+
+        //Bước 1.2: Lấy dữ liệu từ 2 điều khiển
+        String soThuNhat = edtS1.getText().toString();
+        String soThuHai = edtS2.getText().toString();
+        //Bước 1.3: Chuyển dữ liệu từ chuỗi sang số
+        float s1 = Float.parseFloat(soThuNhat);
+        float s2 = Float.parseFloat(soThuHai);
+//Bước 2: Tính toán
+        float Tich = s1 * s2;
+        //Bước 3: Hiển thị kết quả
+        //B3.1: Tìm và tham chiếu
+
+        //B3.2: Chuẩn bị dữ liệu xuất => chuyển KQ thành dạng chuỗi
+        String KQ = String.valueOf(Tich);
+        //B3.3: Gán kết quả lên điều khiển
+        edtKQ.setText(KQ);
+    }
+
+    //Xử lý chia
+    void XuLyChia() {
+        //Bước 1: Lấy dữ liệu
+        //Bước 1.1: Tìm và Tham chiếu dữ liệu
+
+        //Bước 1.2: Lấy dữ liệu từ 2 điều khiển
+        String soThuNhat = edtS1.getText().toString();
+        String soThuHai = edtS2.getText().toString();
+        //Bước 1.3: Chuyển dữ liệu từ chuỗi sang số
+        float s1 = Float.parseFloat(soThuNhat);
+        float s2 = Float.parseFloat(soThuHai);
+        //Bước 2: Tính toán
+        float Thuong = s1 / s2;
+        //Bước 3: Hiển thị kết quả
+        //B3.1: Tìm và tham chiếu
+
+        //B3.2: Chuẩn bị dữ liệu xuất => chuyển KQ thành dạng chuỗi
+        String KQ = String.valueOf(Thuong);
+        //B3.3: Gán kết quả lên điều khiển
+        edtKQ.setText(KQ);
+    }
+}
